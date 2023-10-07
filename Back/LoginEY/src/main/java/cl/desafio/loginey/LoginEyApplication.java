ce755aaa8877299ff790d2413087639a98c5d4c2
Felipe Torres <felipe.torres@valposystems.com>
2023-10-06 23:25:26 -0300

    empezar Desafio EY

---
+++ Back/LoginEY/src/main/java/cl/desafio/loginey/LoginEyApplication.java
@@ -0,0 +1,13 @@
+package cl.desafio.loginey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginEyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginEyApplication.class, args);
+  }
+
+}
